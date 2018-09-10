31c4613ed06c1db40f72d862387b153b53f0e722
Jason Chen <jasonc.piano@gmail.com>
2018-09-09 22:25:59 -0400

    Fix move constructor for state

--- state.cpp
+++ state.cpp
@@ -68,10 +68,10 @@ State::State(const State& prev, PieceId piece, const Card *card, Move move):
 State::State(State&& other):
     player1(other.player1),
     player2(other.player2),
-    board(player1, player2)
-{
-    nextCard = nullptr;
-}
+    board(player1, player2),
+    nextCard(other.nextCard),
+    currPlayer(other.currPlayer)
+{}
 
 string State::render() {
     string currPlayerStr = (currPlayer == PlayerId::P1) ? "P1" : "P2";
